e6bdc62ca821590302727f01c93237d5cce7bef1
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-02 18:20:18 +0100

    Add Exe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 const buttonElement = document.getElementById('counterButton');
 counter = 0;
+let stop = false;
 
 buttonElement.addEventListener( 'click', function () {
-    counter++;
-    buttonElement.innerHTML = `# ${counter}`;
+
+    if(stop = true) {
+        if (counter < 10) {
+            counter++;
+            buttonElement.innerHTML = `# ${counter}`;
+        } else {
+                setTimeout (function() {
+                counter = 0;
+                buttonElement.innerHTML = `# ${counter}`;
+                },2000);
+        }
+    }
+
 });
+
+
+
+//     if (counter === 10) {
+//     setTimeout (function() {
+//         console.log('stop');
+// },2000);
+//     }
+    
+// });
+
+
